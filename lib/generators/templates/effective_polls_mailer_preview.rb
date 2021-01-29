d5d979e1b8b2700bec46c457460f5baa05ef7187
Matt Riemer <matthew@codeandeffect.com>
2021-01-29 10:35:16 -0700

    update mailer preview

--- lib/generators/templates/effective_polls_mailer_preview.rb
+++ lib/generators/templates/effective_polls_mailer_preview.rb
@@ -4,28 +4,36 @@ class EffectivePollsMailerPreview < ActionMailer::Preview
 
   def poll_upcoming_reminder
     poll_notification = build_poll_notification('The poll will start soon')
-    Effective::PollsMailer.poll_upcoming_reminder(poll_notification, bccs)
+    Effective::PollsMailer.poll_upcoming_reminder(poll_notification, build_user)
   end
 
   def poll_start
     poll_notification = build_poll_notification('The poll has started')
-    Effective::PollsMailer.poll_start(poll_notification, bccs)
+    Effective::PollsMailer.poll_start(poll_notification, build_user)
   end
 
   def poll_reminder
     poll_notification = build_poll_notification('The poll started already and you have not participated')
-    Effective::PollsMailer.poll_reminder(poll_notification, bccs)
+    Effective::PollsMailer.poll_reminder(poll_notification, build_user)
   end
 
   def poll_end
     poll_notification = build_poll_notification('The poll has ended')
-    Effective::PollsMailer.poll_end(poll_notification, bccs)
+    Effective::PollsMailer.poll_end(poll_notification, build_user)
   end
 
   protected
 
-  def bccs
-    ['one@example.com', 'two@example.com', 'three@example.com', 'two-hundred-fifty@example.com']
+  def build_user
+    User.new(email: 'buyer@website.com').tap do |user|
+      user.name = 'Valued Customer' if user.respond_to?(:name=)
+      user.full_name = 'Valued Customer' if user.respond_to?(:full_name=)
+
+      if user.respond_to?(:first_name=) && user.respond_to?(:last_name=)
+        user.first_name = 'Valued'
+        user.last_name = 'Customer'
+      end
+    end
   end
 
   def build_poll_notification(text)
