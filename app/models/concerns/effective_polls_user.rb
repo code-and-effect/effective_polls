ee67d6d9f5a13e66bc5053a20a72445a6bfe618f
Matt Riemer <matthew@codeandeffect.com>
2023-11-06 14:54:22 -0700

    Add roles collection to effective_polls

--- app/models/concerns/effective_polls_user.rb
+++ app/models/concerns/effective_polls_user.rb
@@ -21,6 +21,9 @@ module EffectivePollsUser
 
   # The list of all available audience scopes for the Poll Selected Users
   def poll_audience_scopes
+  end
+
+  def default_poll_audience_scopes
     scopes = [
       ['All Users', :all]
     ]
@@ -39,6 +42,12 @@ module EffectivePollsUser
       end
     end
 
+    if self.class.try(:acts_as_role_restricted?)
+      scopes += EffectiveRoles.roles.map do |role|
+        ["All #{role} role users", "with_role_id_#{role}"]
+      end
+    end
+
     scopes
   end
 
@@ -46,18 +55,21 @@ module EffectivePollsUser
   def poll_audience_scope(value)
     collection = self.class.respond_to?(:unarchived) ? self.class.unarchived : self.class
 
-    # If we respond to the fill value, call it
-    return collection.send(value) if collection.respond_to?(value)
-
     # Parse the value
     name, id = value.to_s.split('_id_')
 
     case name.try(:to_sym)
     when :members_with_category
-      collection.members_with_category(EffectiveMemberships.Category.find(id))
-    else
-      raise("unknown poll_audience_scope for #{value}")
+      return collection.members_with_category(EffectiveMemberships.Category.find(id))
+    when :with_role
+      return collection.with_role(id)
     end
+
+    # Otherwise we don't know what this scope is
+    raise("unknown poll_audience_scope for #{value}. Expected #{self.class.name} to have a scope named #{value}") unless collection.respond_to?(value) 
+
+    # If we respond to the fill value, call it
+    collection.send(value) 
   end
 
   def available_polls
