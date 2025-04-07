c07bf5f42b073409ee9801aa5c9f4cd11421e66b
Matt Riemer <matthew@codeandeffect.com>
2025-04-07 14:25:58 -0600

    Datatable followup questions

--- app/datatables/admin/effective_poll_questions_datatable.rb
+++ app/datatables/admin/effective_poll_questions_datatable.rb
@@ -6,23 +6,40 @@ class Admin::EffectivePollQuestionsDatatable < Effective::Datatable
     col :created_at, visible: false
     col :id, visible: false
 
-    col :poll
+    if attributes[:follow_up]
+      col :show_if_value, label: 'When answered with'
+    else
+      col :poll
+    end
 
-    col :position do |poll_question|
+    col :position, visible: false do |poll_question|
       poll_question.position.to_i + 1
     end
 
     col :title
-    col :body, as: :text
-    col :required
+    col :body, as: :text, visible: !attributes[:follow_up]
+    col :required, visible: false
 
     col :category, label: 'Type'
     col :poll_question_options, label: 'Options'
+
+    unless attributes[:follow_up]
+      col :follow_up_poll_questions, action: false, label: 'Follow up questions'
+    end
 
     actions_col
   end
 
   collection do
-    Effective::PollQuestion.all.deep
+    scope = Effective::PollQuestion.all.deep
+
+    if attributes[:follow_up]
+      scope = scope.where(follow_up: true, poll_question_id: attributes[:poll_question_id])
+    else
+      scope = scope.where(poll_question_id: nil)
+    end
+
+    scope
   end
+
 end
--- app/models/effective/poll_question.rb
+++ app/models/effective/poll_question.rb
@@ -2,8 +2,13 @@ module Effective
   class PollQuestion < ActiveRecord::Base
     belongs_to :poll
 
+    belongs_to :poll_question, optional: true # Present when I'm a follow up question
+    belongs_to :poll_question_option, optional: true # Might be present when I'm a follow up question
+
     has_many :poll_question_options, -> { order(:position) }, inverse_of: :poll_question, dependent: :delete_all
     accepts_nested_attributes_for :poll_question_options, reject_if: :all_blank, allow_destroy: true
+
+    has_many :follow_up_poll_questions, class_name: 'Effective::PollQuestion', foreign_key: :poll_question_id, dependent: :destroy
 
     has_rich_text :body
     log_changes(to: :poll) if respond_to?(:log_changes)
@@ -41,20 +46,36 @@ module Effective
 
       position      :integer
 
+      follow_up        :boolean
+      follow_up_value  :string
+
       timestamps
     end
 
-    before_validation(if: -> { poll.present? }) do
-      self.position ||= (poll.poll_questions.map { |obj| obj.position }.compact.max || -1) + 1
+    before_validation(if: -> { poll_question.present? }) do
+      assign_attributes(poll: poll_question.poll)
+    end
+
+    # Set position
+    before_validation do
+      source = poll_question_option.try(:follow_up_poll_questions) || poll.try(:poll_questions) || []
+      self.position = (source.map { |obj| obj.position }.compact.max || -1) + 1
     end
 
     scope :deep, -> { with_rich_text_body_and_embeds.includes(:poll_question_options) }
     scope :sorted, -> { order(:position) }
 
+    scope :top_level, -> { where(follow_up: false) }
+    scope :follow_up, -> { where(follow_up: true) }
+
     validates :title, presence: true
     validates :category, presence: true, inclusion: { in: CATEGORIES }
     validates :position, presence: true
     validates :poll_question_options, presence: true, if: -> { poll_question_option? }
+
+    validates :poll_question, presence: true, if: -> { follow_up? }
+    validates :poll_question_option, presence: true, if: -> { follow_up? && poll_question.try(:poll_question_option?) }
+    validates :follow_up_value, presence: true, if: -> { follow_up? && !poll_question.try(:poll_question_option?) }
 
     # Create choose_one? and select_all_that_apply? methods for each category
     CATEGORIES.each do |category|
@@ -63,6 +84,14 @@ module Effective
 
     def to_s
       title.presence || model_name.human
+    end
+
+    def show_if_value
+      if poll_question.try(:poll_question_option?)
+        poll_question_option.to_s
+      else
+        follow_up_value
+      end
     end
 
     def poll_question_option?
